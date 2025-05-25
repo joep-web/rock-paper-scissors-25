f98df82ace3e04d9b94e9db6d35b1151f01b8430
Joeprey Dangkulos <133377772+joep-web@users.noreply.github.com>
2025-05-25 19:37:12 +0800

    making functionalities

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,54 @@
 const myScore = document.getElementById("my-score")
 const enemyScore = document.getElementById("en-score")
 const roundCount = document.getElementsByClassName("roundsS")
-const name = document.querySelector("#name-area")
+const nameP = document.querySelector("#name-area")
+
 // buttons
 const handsParent = document.querySelector(".choices-cont")
 const shootHand = document.querySelector("#shoot-hand")
 const startGame = document.querySelector("#start")
+
 //display hand cont
 const myHandRes = document.querySelector("#hResult")
 const enemyHandRes = document.querySelector("#cResult")
+
+//other elements
+const grayOver = document.querySelector(".gray-overlay")
+const nameCont = document.querySelector(".name-cont")
+
+//overlays
+
 
 /*  start game
     update the myscore text to name + score
 */
 
+
+
+// CLICK EVENTS
+
 startGame.addEventListener("click", e => {
-    // hide overlays
+    myScore.innerText = `${nameP.value}'s Score: `
+    fadeOutEl(grayOver)
+    fadeOutEl(nameCont)
 })
-
 handsParent.addEventListener("click", e => {
+    // lalabas round n pop up
     // scale yung na click na hand always, unscale yung iba pag nag click ng iba
+    // may barrier na bababa or 3 hands na umiikot as loading shi
+})
+
+shootHand.addEventListener("click", e => {
+    // may pop up na rock paper scissors shoot tag iisang word bababa or pop up
+    // mawawala yung loading dun sa 2 divs
 })
 
 
+// FUNCTIONS
+
+const fadeOutEl = (el) => {
+    el.classList.add('fade-out')
+    el.addEventListener('animationend', () => {
+        el.style.display = 'none'
+    })
+}
