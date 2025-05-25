256c26348e7bd06758b57a4a370834303a3e568b
Joeprey Dangkulos <133377772+joep-web@users.noreply.github.com>
2025-05-25 16:21:08 +0800

    added responsiveness

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+// texts 
+const myScore = document.getElementById("my-score")
+const enemyScore = document.getElementById("en-score")
+const roundCount = document.getElementsByClassName("roundsS")
+const name = document.querySelector("#name-area")
+// buttons
+const handsParent = document.querySelector(".choices-cont")
+const shootHand = document.querySelector("#shoot-hand")
+const startGame = document.querySelector("#start")
+//display hand cont
+const myHandRes = document.querySelector("#hResult")
+const enemyHandRes = document.querySelector("#cResult")
+
+/*  start game
+    update the myscore text to name + score
+*/
+
+startGame.addEventListener("click", e => {
+    // hide overlays
+})
+
+handsParent.addEventListener("click", e => {
+    // scale yung na click na hand always, unscale yung iba pag nag click ng iba
+})
+
+
