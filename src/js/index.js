6d8fd4a580535693e269d3769b4ae6dd4694a50f
Joeprey Dangkulos <133377772+joep-web@users.noreply.github.com>
2025-05-31 15:36:03 +0800

    added loading img choices when you click one of the hands

--- src/js/index.js
+++ src/js/index.js
@@ -44,6 +44,11 @@ shootHand.addEventListener("click", e => {
     rNum++
     roundNumSpan.innerText = rNum
     triggerRoundPopup(rNum)
+
+    triggerRPS("Rock", 1000)
+    triggerRPS("Paper", 1500)
+    triggerRPS("Scissors", 2000)
+    triggerRPS("Shoot", 2500)
     // may pop up na rock paper scissors shoot tag iisang word bababa or pop up
     // mawawala yung loading dun sa 2 divs
 })
@@ -59,19 +64,40 @@ const fadeOutEl = (el) => {
 }
 
 const triggerRoundPopup = (roundNumber) => {
-    const popup = document.createElement('div');
-    popup.className = 'round-popup';
-    popup.textContent = `ROUND ${roundNumber}`;
+    const popup = document.createElement('div')
+    popup.className = 'round-popup'
+    popup.textContent = `ROUND ${roundNumber}`
 
-    document.body.appendChild(popup);
+    document.body.appendChild(popup)
 
     return new Promise((resolve) => {
         setTimeout(() => {
-            popup.classList.add('hide');
+            popup.classList.add('hide')
             setTimeout(() => {
-                popup.remove();
-                resolve();
-            }, 500); // mag wwait ng .5 sec before iremove sa dom yung pop up
-        }, 1800); // mag wwait ng 1.8 sec before ihide yung ppop up
+                popup.remove()
+                resolve()
+            }, 500) // mag wwait ng .5 sec before iremove sa dom yung pop up
+        }, 1000) // mag wwait ng 1.8 sec before ihide yung ppop up
+    });
+}
+
+const triggerRPS = (RPC, time) => {
+    const popup = document.createElement('div')
+   
+    popup.textContent = RPC == "Shoot"? "Shoot !!": `${RPC}`
+    
+    setTimeout(() => {
+        document.body.appendChild(popup)
+        popup.className = `round-popup ${RPC}` // obv the time param is to delay the addition or the pop up of the element
+    }, time)
+ 
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            popup.classList.add('hide')
+            setTimeout(() => {
+                popup.remove()
+                resolve()
+            }, 500) // .5s bago iremove sa dom after ihide 
+        }, time + 2000) // 2 secs bago iplay yung hide animation
     });
 }
